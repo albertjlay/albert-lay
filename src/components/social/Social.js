6c9ae774d77939cf04f2d127134003393e98dd9d
Albert Lay <albertjlay@gmail.com>
2021-05-31 21:22:48 +0700

    Add social component

---
+++ src/components/social/Social.js
@@ -0,0 +1,12 @@
+const Social = (props) => {
+  return (
+    <li>
+      <a href={props.data.link} target="_blank">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" alt={props.data.name}>
+          <path fill-rule="evenodd" d={props.data.svgPath} />
+        </svg>
+      </a>
+    </li>
+  );
+};
+export default Social;
