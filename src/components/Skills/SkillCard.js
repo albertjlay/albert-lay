d3ef485267d6b4939c1c31512d0343d4d36e026a
Albert Lay <albertjlay@gmail.com>
2021-06-01 18:53:13 +0700

    Add skills section

---
+++ src/components/Skills/SkillCard.js
@@ -0,0 +1,43 @@
+import styles from './SkillCard.module.css';
+
+/**
+ * Calculates the time elapsed since startDate (rounded).
+ * @param {Date} startDate - Date start
+ * @return {Object} Two properties.
+ *   time: {Number} Time elapsed. In years if >= 1 year, months otherwise.
+ *   unit: {String} Unit used.
+ */
+const timeElapsed = (startDate) => {
+  const retval = {
+    time: Math.round((Date.now() - startDate) / (30 * 3600 * 24 * 1000)),
+    unit: 'months',
+  };
+  if (retval.time >= 24) {
+    retval.time = retval.time / 12;
+    retval.unit = 'years';
+  } else if (retval.time >= 12) {
+    retval.time = retval.time / 12;
+    retval.unit = 'year';
+  } else if (retval.time <= 1) {
+    retval.unit = 'month';
+  }
+  return retval;
+};
+
+const SkillCard = (props) => {
+  return (
+    <li className={styles['skill-card']}>
+      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <title>{props.data.name}</title>
+        <path d={props.data.svgPath} />
+      </svg>
+      <div className={styles.content}>
+        <div className={styles.name}>{props.data.name}</div>
+        <div className={styles.duration}>{`${timeElapsed(props.data.date).time} ${
+          timeElapsed(props.data.date).unit
+        }`}</div>
+      </div>
+    </li>
+  );
+};
+export default SkillCard;
