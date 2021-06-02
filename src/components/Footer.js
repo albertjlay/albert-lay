c5b9e1a19b0673c52f83fa17d61de67d33b0ce4e
Albert Lay <albertjlay@gmail.com>
2021-06-02 13:14:13 +0700

    Add click handlers to both chevrons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,25 @@
 import styles from './Footer.module.css';
 import SocialList from './social/SocialList';
+import { animateScroll as scroll } from 'react-scroll';
 
 const Footer = () => {
+  const chevronsClickHandler = () => {
+    scroll.scrollToTop({
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+    });
+  };
+
   return (
     <footer>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        onClick={chevronsClickHandler}
+      >
         <path
           fill-rule="evenodd"
           d="M12,12.4142136 L6.70710678,17.7071068 L5.29289322,16.2928932 L12,9.58578644 L18.7071068,16.2928932 L17.2928932,17.7071068 L12,12.4142136 Z M6.70710678,12.7071068 L5.29289322,11.2928932 L12,4.58578644 L18.7071068,11.2928932 L17.2928932,12.7071068 L12,7.41421356 L6.70710678,12.7071068 Z"
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import styles from './Home.module.css';
 import ReactRotatingText from 'react-rotating-text';
 import SocialList from './social/SocialList';
+import { scroller } from 'react-scroll';
 
 const rotatingTextData = [
   'Student software engineer',
@@ -13,6 +14,10 @@ const rotatingTextData = [
 ];
 
 const Home = () => {
+  const chevronsClickHandler = () => {
+    scroller.scrollTo('about', { duration: 1500, delay: 100, smooth: true, offset: -90 });
+  };
+
   return (
     <div id="home" className={styles.wrapper}>
       <main>
@@ -32,7 +37,8 @@ const Home = () => {
         <div className={styles.socials}>
           <SocialList />
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onClick={chevronsClickHandler}>
           <path
             fill-rule="evenodd"
             d="M17.2928932,10.2928932 L18.7071068,11.7071068 L12,18.4142136 L5.29289322,11.7071068 L6.70710678,10.2928932 L12,15.5857864 L17.2928932,10.2928932 Z M12,10.5857864 L17.2928932,5.29289322 L18.7071068,6.70710678 L12,13.4142136 L5.29289322,6.70710678 L6.70710678,5.29289322 L12,10.5857864 Z"
