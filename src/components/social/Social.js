bdb4d9e1c1d02f80828f5a4ee104854ea48aadbc
Albert Lay <albertjlay@gmail.com>
2021-06-02 11:35:15 +0700

    Add noreferrer tag to Social component

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -1,7 +1,7 @@
 const Social = (props) => {
   return (
     <li>
-      <a href={props.data.link} target="_blank">
+      <a href={props.data.link} target="_blank" rel="noreferrer">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" alt={props.data.name}>
           <path fill-rule="evenodd" d={props.data.svgPath} />
         </svg>
