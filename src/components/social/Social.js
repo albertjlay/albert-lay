8bf8c0c8cbf5b1c42b8cd2a81e3a3842a1f717aa
Albert Lay <albertjlay@gmail.com>
2021-06-13 11:17:38 +0700

    Add hidden link name to socials for accessibility

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -1,10 +1,13 @@
+import styles from './Social.module.scss';
+
 const Social = (props) => {
   return (
     <li>
-      <a href={props.data.link} target="_blank" rel="noreferrer">
+      <a href={props.data.link} target="_blank" rel="noreferrer" className={styles.social}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" alt={props.data.name}>
           <path fillRule="evenodd" d={props.data.svgPath} />
         </svg>
+        {props.data.name}
       </a>
     </li>
   );
